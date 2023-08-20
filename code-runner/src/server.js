2e4433fc1b3fcc716630b4857ed0bc152b807dab
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-08-20 11:46:29 +0200

    fix: add cors

--- code-runner/src/server.js
+++ code-runner/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require("./worker.js");
 
 const app = express();
 
 const PORT = 4000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'code-runner is running correctly' });
